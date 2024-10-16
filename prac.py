4e77dade8413b8597d1b69df85e9cd96203d1be5
Min Thwin Khant <minthwin2000@gmail.com>
2024-10-15 23:27:07 -0700

    First Commit

---
+++ prac.py
@@ -0,0 +1,26 @@
+def decorator_one(func):
+    print("enters 1")
+    def wrapper(*args, **kwargs):
+        print("Decorator 1 wrapper STARTS")
+        result = func(*args, **kwargs)
+        print("Decorator 1 wrapper ENDS")
+        return result
+    print("exits 1")
+    return wrapper
+
+def decorator_two(func):
+    print("enters 2")
+    def wrapper(*args, **kwargs):
+        print("Decorator 2 wrapper STARTS")
+        result = func(*args, **kwargs)
+        print("Decorator 2 wrapper ENDS")
+        return result
+    print("exits 2")
+    return wrapper
+
+@decorator_one
+@decorator_two
+def greet():
+    print("Hello!")
+
+greet()
